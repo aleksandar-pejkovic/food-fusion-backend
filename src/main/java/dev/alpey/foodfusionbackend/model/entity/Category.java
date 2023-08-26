6c8a485903c019fdb0d82e41e18d21dbedacbf40
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-08-26 15:03:24 +0200

    food - category relation

--- src/main/java/dev/alpey/foodfusionbackend/model/entity/Category.java
+++ src/main/java/dev/alpey/foodfusionbackend/model/entity/Category.java
@@ -1,9 +1,14 @@
 package dev.alpey.foodfusionbackend.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,4 +28,9 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Food> foodList = new ArrayList<>();
 }
--- src/main/java/dev/alpey/foodfusionbackend/model/entity/Food.java
+++ src/main/java/dev/alpey/foodfusionbackend/model/entity/Food.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -28,7 +30,13 @@ public class Food {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
     private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 
     @OneToMany(mappedBy = "food")
     private List<Item> itemList = new ArrayList<>();
